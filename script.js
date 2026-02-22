863aeb2153beda631e8a30019055d8697b2a6c79
hassanharunani7 <hassanharunani7@gmail.com>
2026-02-22 15:37:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1089,6 +1089,28 @@ if (document.body.classList.contains("dark-mode")) {
 }
 
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
 
 
 
